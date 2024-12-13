2abe105024f4156d8ff872b9f1af4393459fd7ca
rehanjaffar <rehanmathrooma@gmail.com>
2024-12-13 08:59:37 +0500

    commit

--- src/components/testimonial/Testimonials.jsx
+++ src/components/testimonial/Testimonials.jsx
@@ -32,20 +32,20 @@ const Testimonials = () => {
           data-aos="fade-up"
           loop={true}
           spaceBetween={40}
-          slidesPerView={1.7}
+          slidesPerView={1.5}
           onSlideChange={(e) => {
           
             setActiveIndex(e.realIndex);
           }}
           modules={[Pagination,Autoplay]}
           autoplay={true}
-          className="md:h-96 h-[40rem] max-w-3xl "
+          className="md:h-96 h-[28rem] sm:max-w-3xl "
         >
           {testimonials.testimonials_content.map((content, i) => {
             return (
               <SwiperSlide key={i}>
                 <div
-                  className={`duration-500 bg-bg_light_primary mx-8 border-2 p-8 h-full rounded-2xl flex items-center gap-6 border-slate-200 selection-none md:flex-row flex-col ${
+                  className={`duration-500 bg-bg_light_primary sm:mx-8 border-2 p-8 h-full w-[90%] rounded-2xl flex items-center gap-6 border-slate-200 selection-none md:flex-row flex-col ${
                     activeIndex !== i && "scale-75 blur-sm"
                   }`}
                 >
