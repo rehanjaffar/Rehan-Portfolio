19918204f0032d74f6bae8560f7de12a51a6402d
rehanjaffar <rehanmathrooma@gmail.com>
2025-09-02 22:15:04 +0500

    hero section update

--- src/components/hero/HeroData.jsx
+++ src/components/hero/HeroData.jsx
@@ -1,18 +1,18 @@
 const Hero_person = "/assets/images/Hero/profile.png";
 
 export const hero = {
-    title: "MERN STACK DEVELOPER",
+    title: "Full-Stack Developer with AI/ML Skills",
     firstName: "REHAN",
     LastName: "JAFFAR",
     btnText: "Hire Me",
     image: Hero_person,
     hero_content: [
       {
-        count: "0",
+        count: "3",
         text: "Year of Experinse in Web development",
       },
       {
-        count: "5+",
+        count: "25+",
         text: "Projects Worked in my career",
       },
     ]
